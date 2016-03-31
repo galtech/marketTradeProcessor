8e65c932d422e977057be24f234151c04f70620f
Peter Heylin <peter.heylin@ie.fujitsu.com>
2016-03-31 16:44:13 +0100

    20160331 data transfer screen

--- index.php
+++ index.php
@@ -28,8 +28,6 @@
 
 </form>
 
-<?php echo '<p style="clear:both;">&copy;' . date('Y',time()) . ' Peter Heylin</p>'; ?>
-
 </body>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>

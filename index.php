4db75cda47fdea5e23f9c7794cd7449bcd624844
Peter Heylin <peter.heylin@ie.fujitsu.com>
2016-03-31 16:42:47 +0100

    20160331 data transfer screen

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
+<body>
 
 <form name="postMsg" method="post" action="post_data.php" class="form-control">
-<input type="text" name="userID" id="userID" value="134256" readonly="readonly" class="form-control"/><br/>
+<input type="text" name="userID" id="userID" value="134256" readonly="readonly" class="form-control" style="width:25%;"/><br/>
 <label>Currency From</label>
 <select name="currencyFrom" id="currencyFrom">
 <option value="EUR">Euro</option>
@@ -20,16 +21,16 @@
 <option value="USD">US Dollar</option>
 <option value="GBP">UK Pound</option>
 </select><br/>
-<label>Amount to Sell</label><input type="text" name="amountSell" id="amountSell" class="form-control"/><br/>
-<label>Amount to Buy</label><input type="text" name="amountBuy" id="amountBuy" class="form-control"/><br/>
+<label>Amount to Sell</label><input type="text" name="amountSell" id="amountSell" class="form-control" style="width:25%;"/><br/>
+<label>Amount to Buy</label><input type="text" name="amountBuy" id="amountBuy" class="form-control" style="width:25%;"/><br/>
 <label>Rate</label><input type="text" name="rate" id="rate" value="0.7471"/><br/>
-<input type="text" name="origCountry" id="origCountry" value="IE" readonly="readonly"/><br/>
+<label>Originating Country</label><input type="text" name="origCountry" id="origCountry" value="IE" readonly="readonly"/><br/>
 
 </form>
 
-<?php echo '&copy;' . date('Y',time()) . ' Peter Heylin'; ?>
+<?php echo '<p style="clear:both;">&copy;' . date('Y',time()) . ' Peter Heylin</p>'; ?>
 
-
+</body>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </html>
